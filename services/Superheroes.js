75efaa2534eaaa033ad38a792eecc30e163589b4
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-11 15:23:49 -0400

    made superhero controller, service, search, and background color change

---
+++ services/Superheroes.js
@@ -0,0 +1,38 @@
+/* global angular */
+
+(function () {
+  angular
+    .module('superheroApp')
+    .factory('superheros', [superherosService])
+
+  function superherosService () {
+    const superheros = [
+      {
+        name: 'Superman',
+        phonenumber: '555-555-5557',
+        photo_url: '',
+        affiliation: 'DC Comics'
+      },
+      {
+        name: 'Batman',
+        phonenumber: '555-555-5557',
+        photo_url: '',
+        affiliation: 'DC Comics'
+      },
+      {
+        name: 'Iron Man',
+        phonenumber: '555-555-5557',
+        photo_url: '',
+        affiliation: 'Marvel'
+      }
+    ]
+
+    return {
+      all: all
+    }
+
+    function all () {
+      return superheros
+    }
+  } // end SuperherosService
+})()
