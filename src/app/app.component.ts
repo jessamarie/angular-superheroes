c97e1101011b2dee8c0f1ebc9149413681260640
Jessa <jessamarie@users.noreply.github.com>
2017-12-26 18:04:20 -0500

    fixed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Superheros Phone book';
+  title = 'Superhero Phone Book';
 }
